9785a8d0661b5bd1aef8daa39c182f187a4b49ec
nisrina salsa <nisrinahikmawan@gmail.com>
2025-10-23 21:58:01 +0700

    Menambahkan Card kedua, Card ketiga, dan Menambahkan warna DarkGreen

--- app/src/main/java/com/example/questadvancelayout/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout/UIPertama.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
@@ -54,17 +55,91 @@ fun ActivitasPertama(modifier: Modifier){
                 Spacer(modifier = Modifier.width(30.dp))
                 Column() {
                     Text(
-                        stringResource(R.string.nama),
+                        stringResource(R.string.namaBox1),
                         fontSize = 30.sp,
                         fontFamily = FontFamily.Cursive,
                         color = Color.White,
                         modifier = Modifier.padding(top=15.dp)
                     )
                     Text(
-                        stringResource(R.string.alamat),
+                        stringResource(R.string.alamatBox1),
                         fontSize = 20.sp,
                         color = Color.Yellow,
                         modifier = Modifier.padding(top=10.dp)
+                    )
+                }
+            }
+        }
+        Card(modifier = Modifier
+            .fillMaxWidth(1f)
+            .padding(12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.Blue
+            )){
+            Row() {
+                val gambar = painterResource(R.drawable.logo)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier.size(115.dp).padding(10.dp)
+                )
+                Spacer(modifier = Modifier.width(30.dp))
+                Column() {
+                    Text(
+                        stringResource(R.string.namaBox2),
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White,
+                        modifier = Modifier.padding(top=15.dp)
+                    )
+                    Text(
+                        stringResource(R.string.telp),
+                        fontSize = 20.sp,
+                        color = Color.Cyan,
+                        modifier = Modifier.padding(top=5.dp)
+                    )
+                    Text(
+                        stringResource(R.string.alamatBox2),
+                        fontSize = 20.sp,
+                        color = Color.White,
+                        modifier = Modifier.padding(top=5.dp, bottom = 10.dp)
+                    )
+                }
+            }
+        }
+        Card(modifier = Modifier
+            .fillMaxWidth(1f)
+            .padding(12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = colorResource(id = R.color.DarkGreen)
+            )){
+            Row() {
+                val gambar = painterResource(R.drawable.logo)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier.size(115.dp).padding(10.dp)
+                )
+                Spacer(modifier = Modifier.width(30.dp))
+                Column() {
+                    Text(
+                        stringResource(R.string.namaBox3),
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White,
+                        modifier = Modifier.padding(top=15.dp)
+                    )
+                    Text(
+                        stringResource(R.string.telp),
+                        fontSize = 20.sp,
+                        color = Color.Cyan,
+                        modifier = Modifier.padding(top=5.dp)
+                    )
+                    Text(
+                        stringResource(R.string.alamatBox3),
+                        fontSize = 20.sp,
+                        color = Color.White,
+                        modifier = Modifier.padding(top=5.dp, bottom = 10.dp)
                     )
                 }
             }
