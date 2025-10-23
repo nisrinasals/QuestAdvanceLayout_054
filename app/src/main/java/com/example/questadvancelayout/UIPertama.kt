9598ea37ca2a80fd174151486c6c9be154cf3ff6
nisrina salsa <nisrinahikmawan@gmail.com>
2025-10-23 11:26:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/questadvancelayout/UIPertama.kt
@@ -0,0 +1,9 @@
+package com.example.questadvancelayout
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
