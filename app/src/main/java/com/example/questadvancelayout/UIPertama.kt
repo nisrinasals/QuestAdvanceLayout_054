8da7d1283f30652a199320a68a51371f7198f8bd
nisrina salsa <nisrinahikmawan@gmail.com>
2025-10-23 12:20:03 +0700

    menambahkan box

--- app/src/main/java/com/example/questadvancelayout/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout/UIPertama.kt
@@ -1,6 +1,7 @@
 package com.example.questadvancelayout
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -68,6 +69,9 @@ fun ActivitasPertama(modifier: Modifier){
                 }
             }
         }
-
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        )
     }
 }
