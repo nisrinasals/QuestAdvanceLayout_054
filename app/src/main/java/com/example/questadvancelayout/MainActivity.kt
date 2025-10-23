3474ec550aee553e63a1d698e32968fe18aa7e86
nisrina salsa <nisrinahikmawan@gmail.com>
2025-10-23 12:29:19 +0700

    Memanggil fun ActivitasPertama pada MainActivity

--- app/src/main/java/com/example/questadvancelayout/MainActivity.kt
+++ app/src/main/java/com/example/questadvancelayout/MainActivity.kt
@@ -16,32 +16,14 @@ import com.example.questadvancelayout.ui.theme.QuestAdvanceLayoutTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             QuestAdvanceLayoutTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ActivitasPertama(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestAdvanceLayoutTheme {
-        Greeting("Android")
     }
 }
