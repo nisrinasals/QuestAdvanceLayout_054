0c552674778de612eae03b635b19bcea1c83546f
nisrina salsa <nisrinahikmawan@gmail.com>
2025-10-23 11:34:58 +0700

    menambahkan teks prodi

--- app/src/main/java/com/example/questadvancelayout/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout/UIPertama.kt
@@ -6,13 +6,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 
 @Composable
 fun ActivitasPertama(modifier: Modifier){
-    Column(modifier = Modifer.padding(top=100.dp)
+    Column(modifier = Modifier.padding(top=100.dp)
         .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally)
-    {
-
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+        Text(text= stringResource(id=R.string.prodi),
+            fontsize = 35.sp,
+            fontWeight = FontWeight.Bold)
     }
 }
