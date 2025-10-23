3fe107da3976670ae7eea47f3a78682d0eb4a706
nisrina salsa <nisrinahikmawan@gmail.com>
2025-10-23 12:04:50 +0700

    menambahkan modifier Text nama

--- app/src/main/java/com/example/questadvancelayout/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout/UIPertama.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -53,6 +54,10 @@ fun ActivitasPertama(modifier: Modifier){
                 Column() {
                     Text(
                         stringResource(R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top=15.dp)
                     )
                 }
             }
